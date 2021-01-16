451e59452fad50738a86c3ed9ecedebd8cee43ce
Alan Velazquez <alan2719.ahcv@gmail.com>
2021-01-16 13:57:52 -0600

    added seed file

---
+++ models/car.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+    image: { 
+        type: String, 
+        },
+    carid: { 
+        type: Number, 
+    },
+    maker: {
+        type: String
+    },
+    model: {
+        type: String
+    },
+    kilometers: {
+        type: String
+    },
+    date: { 
+        type: Date, 
+    },
+    description: {
+        type: String
+    }
+});
+
+const Cars = mongoose.model("Cars", carSchema);
+
+module.exports = Cars;
